d094ad7c7320a25595630b86349c299182e71994
Germain Boue <boue.germain@gmail.com>
2015-01-21 17:37:15 -0800

    Add new/create in article controller as well as the private method for article_params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,4 +4,27 @@ class ArticlesController < ApplicationController
 
   end
   
+  def new 
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :content)
+  end
+
+  def find_article
+  end
+
 end
+
