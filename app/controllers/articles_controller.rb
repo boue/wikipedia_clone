81e229d863e28c3056a9bcc1b2f0906152a2db8e
Germain Boue <boue.germain@gmail.com>
2015-01-21 20:51:42 -0800

    Make before_action for show action so that it finds article and stores it in variable

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :find_article, only: [:show]
   
   def index
 
@@ -24,6 +25,7 @@ class ArticlesController < ApplicationController
   end
 
   def find_article
+    @article = Article.find(params[:id])
   end
 
 end
